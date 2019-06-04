f82df926ea30956f39d3efc938d8a5fe0fe9397b
Michael J. Grayling <mjg211@cam.ac.uk>
2019-06-05 00:10:25 +0100

    Added Imports to DESCRIPTION

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(rep("-", 67), "\nph2rand: Design and analysis of ",
-                        "randomized phase II oncology trials\n", rep("-", 67),
+  packageStartupMessage(rep("-", 66), "\nph2rand: Design of randomized ",
+                        "comparative phase II oncology trials\n", rep("-", 66),
                         "\n\nv.0.7: For an overview of the package's ",
                         "functionality enter: ?ph2rand\n\nFor news on the ",
                         "latest updates enter: news(package = \"ph2rand\")")
